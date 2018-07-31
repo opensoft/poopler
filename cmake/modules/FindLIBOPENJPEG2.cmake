b8dc144de3814b2459dfddb2adcdc8730242920d
Aleksey Nikolaev <aleksey.nikolaev@opensoftdev.ru>
2018-07-31 12:14:16 +0300

    Make full path for the openjpeg library

--- cmake/modules/FindLIBOPENJPEG2.cmake
+++ cmake/modules/FindLIBOPENJPEG2.cmake
@@ -17,6 +17,24 @@ set(LIBOPENJPEG2_LIBRARIES)
 
 find_package(PkgConfig REQUIRED)
 pkg_check_modules(LIBOPENJPEG2 libopenjp2)
+
 if (LIBOPENJPEG2_FOUND)
   add_definitions(-DUSE_OPENJPEG2)
+  # This make full path for the library
+  find_library(LIBOPENJPEG2_LIBRARY NAMES ${LIBOPENJPEG2_LIBRARIES}
+               HINTS ${LIBOPENJPEG2_LIBRARY_DIRS}
+  )
+  set(LIBOPENJPEG2_LIBRARIES "${LIBOPENJPEG2_LIBRARY}")
+
+  find_path(LIBOPENJPEG2_INCLUDE_DIR openjpeg.h
+            HINTS ${LIBOPENJPEG2_INCLUDE_DIRS}
+  )
+  set(LIBOPENJPEG2_INCLUDE_DIRS "${LIBOPENJPEG2_INCLUDE_DIR}")
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(LIBOPENJPEG2 DEFAULT_MSG LIBOPENJPEG2_LIBRARIES LIBOPENJPEG2_INCLUDE_DIRS)
+  mark_as_advanced(
+    LIBOPENJPEG2_INCLUDE_DIR
+    LIBOPENJPEG2_LIBRARIES
+  )
 endif ()
